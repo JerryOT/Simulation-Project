16d3ac5f5995eff13ee255f67c9cf91dcdf76b05
Jerry Yang <jerry.yang@ontariotechu.net>
2026-03-18 12:57:45 -0400

    UI

---
+++ Component__UI.py
@@ -0,0 +1,96 @@
+import pygame
+
+Colors = {
+    'BLACK' : (0, 0, 0),
+    'WHITE' : (255, 255, 255),
+    'RED' : (255, 0, 0),
+    'GREEN' : (0, 255, 0),
+    'BLUE' : (0, 0, 255)
+}
+
+def GetColors():
+    return Colors
+
+class MyText():
+    def __init__(self,
+                 color,
+                 background = Colors['BLACK'], antialias=True,
+                 fontname="comicsansms", fontsize=16
+                 ):
+        pygame.font.init()
+        self.font = pygame.font.SysFont(fontname, fontsize)
+        self.string = ""
+        self.pos = (0,0)
+        self.color = color
+        self.background = background
+        self.antialias = antialias
+
+    def SetString(self, s):
+        self.string = s
+
+    def SetPosition(self, x, y):
+        new_x = x
+        new_y = y
+        self.pos = (new_x, new_y)
+    
+    def Draw(self, screen):
+        text = self.font.render(self.string, self.antialias, self.color, self.background)
+        screen.blit(text, self.pos)
+
+
+class Box(pygame.sprite.Sprite):
+    def __init__(self, color, width, height):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.color = color
+        self.pos = (0,0)
+        self.angle = 0
+        self.SetSize(width, height)
+
+    def SetSize(self, width, height):
+        self.image = pygame.Surface([width, height], flags=pygame.SRCALPHA)
+        self.image.fill(self.color)
+
+    def SetRotation(self, angle):
+        self.angle = angle
+
+    def SetPosition(self, x, y):
+        new_x = x
+        new_y = y
+        self.pos = (new_x, new_y)
+
+    def Draw(self, surface, screenWidth, screenHeight):
+        image_rot = pygame.transform.rotate(self.image, self.angle)
+        rect = image_rot.get_rect()
+        rect.center = (self.pos[0] + screenWidth / 2, self.pos[1] + screenHeight / 2)
+        surface.blit(image_rot, rect)
+
+
+class Circle(pygame.sprite.Sprite):
+    def __init__(self, color, radius):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.color = color
+        self.pos = (0,0)
+        self.radius = radius
+        self.SetSize(radius)
+
+    def SetSize(self, radius):
+        size = radius * 2
+        self.image = pygame.Surface((size, size), pygame.SRCALPHA)
+        self.image.fill((0, 0, 0, 0))
+        pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
+
+    def SetPosition(self, x, y):
+        new_x = x
+        new_y = y
+        self.pos = (new_x, new_y)
+
+    def Draw(self, surface, screenWidth, screenHeight):
+        image = self.image
+        rect = image.get_rect()
+        rect.center = (self.pos[0] + screenWidth / 2, self.pos[1] + screenHeight / 2)
+        surface.blit(image, rect)
+
+
+
---
+++ Main.py
@@ -0,0 +1,54 @@
+import pygame, sys
+import matplotlib.pyplot as plt
+import Component__UI
+
+
+def main():
+
+   # initializing pygame
+    #pygame.mixer.init()
+    pygame.init()
+    clock = pygame.time.Clock()
+
+    screen_width = 640
+    screen_height = 640
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    pygame.display.set_caption('Rigid')
+
+    paused = False
+
+    while True:
+        clock.tick(60)
+        screen.fill(Component__UI.GetColors()['BLACK'])
+
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit(0)
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_r:
+            paused = not paused
+        elif event.type == pygame.KEYDOWN and event.key == pygame.K_q:
+            pygame.quit()
+            sys.exit(0)
+        else:
+            pass
+        if paused:
+            continue
+
+
+        #TEST CASE
+        Box_A = Component__UI.Box(Component__UI.GetColors()['RED'], 40, 40)
+        Box_A.SetPosition(0,0)
+        Box_A.Draw(screen, screen_width, screen_height)
+
+        Circle_A = Component__UI.Circle(Component__UI.GetColors()['RED'], 20)
+        Circle_A.SetPosition(80,0)
+        Circle_A.Draw(screen, screen_width, screen_height)
+        
+
+        pygame.display.flip()
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
